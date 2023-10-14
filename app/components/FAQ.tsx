718ee5b7f95a38645485493dae5dd132fe30ab19
SashaBayan <sashabayan@gmail.com>
2023-10-14 11:36:37 -0700

    add copy + FAQ

---
+++ app/components/FAQ.tsx
@@ -0,0 +1,81 @@
+"use client";
+import { useState } from "react";
+
+const faqs = [
+  {
+    question: "What is a Musical Devotional?",
+    answer:
+      "A musical devotional is a gathering where we come together to explore spiritual and philosophical writings through reading and musical expression. It's not only focused on the Baha'i faith but also welcomes and celebrates other belief systems and philosophical viewpoints. It's a cozy, friendly space where we celebrate our shared humanity through words and music, without any preaching or proselytizing.",
+  },
+  {
+    question: "Who can attend a Musical Devotional?",
+    answer:
+      "Absolutely everyone! Whether you’re coming from a specific faith perspective, hold secular viewpoints, or are simply curious, you’re warmly welcome. There is no prerequisite belief or practice needed to join us. It's a judgment-free, inclusive space where all can explore and express their spirituality or philosophical perspectives freely.",
+  },
+  {
+    question: "Do I have to participate or can I just listen?",
+    answer:
+      "Your level of participation is entirely up to you! If you wish to share a piece of writing, prayer, or music, you're most welcome to. If you prefer to simply listen and absorb, that’s completely fine too. The Musical Devotional is meant to be a comfortable space where you can engage in a way that feels right for you.",
+  },
+  {
+    question: "Are there any costs involved?",
+    answer:
+      "No, there are no costs to attend a Musical Devotional. It’s entirely free. Our gatherings are simple and informal, and we want them to be accessible to everyone, regardless of financial means.",
+  },
+  {
+    question: "How do I know the location of the next event?",
+    answer:
+      "Our Musical Devotionals are currently held in Oakland, in the Bay Area. To get the exact location of the next event, please get in touch with us through the contact information provided on our website. We'd be thrilled to welcome you!",
+  },
+];
+
+export function FAQ() {
+  const [expandedIndex, setExpandedIndex] = useState<number | null>(null);
+  return (
+    <section className="mt-12 w-full max-w-2xl text-start">
+      <h2 className="mb-6 text-center text-2xl font-semibold">
+        Frequently Asked Questions
+      </h2>
+      <dl className="space-y-4">
+        {faqs.map((faq, index) => (
+          <div
+            key={index}
+            className="rounded-md border p-4 text-left shadow-sm"
+          >
+            <dt
+              className="items-left flex cursor-pointer justify-between"
+              onClick={() =>
+                setExpandedIndex(expandedIndex === index ? null : index)
+              }
+            >
+              <span className="font-medium">{faq.question}</span>
+              <span
+                className={`transform transition-transform duration-300 ${
+                  expandedIndex === index ? "rotate-180" : ""
+                }`}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M19 9l-7 7-7-7"
+                  />
+                </svg>
+              </span>
+            </dt>
+            {expandedIndex === index && (
+              <dd className="mt-2 text-black">{faq.answer}</dd>
+            )}
+          </div>
+        ))}
+      </dl>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,89 @@
-import Image from "next/image";
+"use client";
 import { BackgroundVideo } from "./components/BackgroundVideo";
+import { FAQ } from "./components/FAQ";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-8 text-center text-gray-800">
       <BackgroundVideo />
+
+      {/* Header */}
+      <h1 className="mb-2 text-4xl font-bold">Musical Devotional</h1>
+
+      {/* Upcoming Event */}
+      <h2 className="mb-2 text-2xl font-semibold">
+        Upcoming Event: October 18th
+      </h2>
+      <div className="mb-4">
+        <p>
+          Join us for our next musical devotional on October 18th at 6:30 PM in
+          Oakland!
+        </p>
+        <p>Dinner will be provided.</p>
+      </div>
+      <a
+        href="mailto:info@musicaldevo.com"
+        className="mb-8 text-blue-500 underline"
+      >
+        Contact us to RSVP
+      </a>
+
+      {/* Sections */}
+      <section className="max-w-2xl space-y-8">
+        {/* Section: Welcome */}
+        <div className="space-y-4">
+          <h2 className="text-2xl font-semibold">
+            Welcome to Our Musical Devotional!
+          </h2>
+          <p>
+            Hey there, and thanks for dropping by! If you’ve stumbled upon our
+            little corner of the internet because you’re curious about what a
+            musical devotional is, you’re in the right place.
+          </p>
+        </div>
+
+        {/* Section: What's a Musical Devotional */}
+        <div className="space-y-4">
+          <h2 className="text-2xl font-semibold">
+            What&apos;s a Musical Devotional Anyway?
+          </h2>
+          <p>
+            Think of a musical devotional as a cozy get-together. It&apos;s
+            where we hang out, share musical offerings, explore writings and
+            prayers from the Bahá&apos;í Faith—-and from other beliefs and
+            philosophies too. Imagine a friendly space where we share, reflect,
+            and celebrate our shared human spirit through words and music. No
+            preaching, just good vibes!
+          </p>
+        </div>
+
+        {/* Section: Our Vision */}
+        <div className="space-y-4">
+          <h2 className="text-2xl font-semibold">Our Vision</h2>
+          <p>
+            We&apos;re all about embracing unity and exploring the spiritual
+            threads that bind us all, no matter where we come from or what we
+            believe. Inspired by Baha&apos;i teachings, we&apos;re here to build
+            a community that digs deeper into understanding, respecting, and
+            celebrating diverse spiritual paths.
+          </p>
+        </div>
+
+        {/* Section: Join Us */}
+        <div className="space-y-4">
+          <h2 className="text-2xl font-semibold">Join Us</h2>
+          <p>
+            Whether you&apos;re into sharing some philosophical wisdom,
+            exploring prayers, playing a tune, or just soaking it all in,
+            we&apos;re stoked to have you with us. It&apos;s all about the
+            journey, and we look forward to exploring, sharing, and connecting
+            with you!
+          </p>
+        </div>
+      </section>
+
+      {/* FAQ Section */}
+      <FAQ />
     </main>
   );
 }
